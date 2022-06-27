d99c3619eb1e72c5467917b3fbe3bb1b11ebe970
sho-ts <rrv1011@icloud.com>
2022-06-28 05:09:26 +0900

    データの永続化

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +10,27 @@ export class HomeComponent implements OnInit {
   task = new FormControl('', []);
   tasks: string[] = [];
 
+  save() {
+    localStorage.setItem('ang-todos', JSON.stringify(this.tasks));
+  }
+
   addTask() {
     this.task.value && this.tasks.unshift(this.task.value);
-    this.task.setValue('')
+    this.task.setValue('');
+    this.save();
   }
 
   handleItemDelete(index: number) {
     this.tasks.splice(index, 1);
+    this.save();
   }
 
-  constructor() {}
+  ngOnInit(): void {
+    const data = localStorage.getItem('ang-todos');
 
-  ngOnInit(): void {}
+    if (data) {
+      const taskData = JSON.parse(data) as string[];
+      this.tasks = taskData;
+    }
+  }
 }
